9f8b91e0c5855caa81fcbe77a8ca2d3b158ecc6d
Sinigaglia Steeve <steeve.sinigaglia@laposte.net>
2017-11-09 13:51:50 +0100

    end

--- app/src/main/java/fr/android/androidexercises/BookActivity.java
+++ app/src/main/java/fr/android/androidexercises/BookActivity.java
@@ -1,5 +1,6 @@
 package fr.android.androidexercises;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 public class BookActivity extends AppCompatActivity {
 
     private String bookName = "Garry Whopper";
+    public static int CODE_BOOK_NAME = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,10 @@ public class BookActivity extends AppCompatActivity {
         sendNameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(bookName);
+                intent.putExtra("bookName",bookName);
+                setResult(CODE_BOOK_NAME, intent);
+                finish();
                 // TODO set result to book name in intent
                 // TODO finish current activity
             }
--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -25,7 +25,7 @@ public class LibraryActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Replace with startActivityForResult() to start BookActivity
-                Toast.makeText(LibraryActivity.this, R.string.toast_todo, Toast.LENGTH_SHORT).show();
+                startActivityForResult(new Intent(LibraryActivity.this, BookActivity.class), BookActivity.CODE_BOOK_NAME);
             }
         });
     }
@@ -33,6 +33,9 @@ public class LibraryActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // TODO get back book name
+        if (resultCode == BookActivity.CODE_BOOK_NAME) {
+            Toast.makeText(this, data.getStringExtra("bookName"), Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
