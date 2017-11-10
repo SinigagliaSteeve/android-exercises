afefbba483124dbada215b2f1c31ff73445b4ce6
Sinigaglia Steeve <steeve.sinigaglia@laposte.net>
2017-11-10 08:50:46 +0100

    picasso

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -2,6 +2,10 @@ package fr.android.androidexercises;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -11,7 +15,13 @@ public class LibraryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_library);
 
         // TODO Get image view and load image form URL
+
+        ImageView downloadImageView = findViewById(R.id.downloadImageView);
+        Picasso.with(this)
+                .load("http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg")
+                .centerCrop()
+                .resize(200,200)
+                .into(downloadImageView);
         // http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg
     }
-
 }
