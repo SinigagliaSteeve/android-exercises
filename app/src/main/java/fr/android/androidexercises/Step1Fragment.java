39d9eb06598ef7fe9ea3ce7226d6b773babe5689
Sinigaglia Steeve <steeve.sinigaglia@laposte.net>
2017-11-10 11:30:53 +0100

    manage fragment

--- app/src/main/java/fr/android/androidexercises/Step1Fragment.java
+++ app/src/main/java/fr/android/androidexercises/Step1Fragment.java
@@ -1,9 +1,22 @@
 package fr.android.androidexercises;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class Step1Fragment extends Fragment {
+    private TextView textView;
 
-    // TODO Override onCreateViewMethod
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_step1, container, false);
+        textView = view.findViewById(R.id.step1TextView);
+        return view;
 
+    }
 }
